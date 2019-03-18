649713f5c723f42c703fe027a33763a1321e6fb8
Rosa Filgueira <rosa.filgueira@ed.ac.uk>
2019-03-18 13:01:24 +0000

    Update spark-interactive-textmining.sh

--- spark-interactive-textmining.sh
+++ spark-interactive-textmining.sh
@@ -15,7 +15,7 @@ NUM=$(wc -l bash_scripts/worker.log)
 NUMWORKERS=$(echo $NUM| cut -d' ' -f1)
 NUMCORES=$( expr 16 '*' "$NUMWORKERS")
 
-export SPARK_HOME=/home/rfilguei/spark-2.4.0-bin-hadoop2.7
+export SPARK_HOME=${HOME}/spark-2.4.0-bin-hadoop2.7
 cd $HOME/defoe
 
 $SPARK_HOME/bin/spark-submit --master spark://$hostmaster:7077 --executor-memory 60g --py-files defoe.zip defoe/run_query.py data.txt papers defoe.papers.queries.total_words -n $NUMCORES > query_job.txt
