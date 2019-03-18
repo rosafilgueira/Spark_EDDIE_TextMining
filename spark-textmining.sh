67bf802b8714c42d14d7c597fe5acb15eb367d1c
Rosa Filgueira <rosa.filgueira@ed.ac.uk>
2019-03-18 11:03:48 +0000

    Update spark-textmining.sh

--- spark-textmining.sh
+++ spark-textmining.sh
@@ -14,4 +14,4 @@ NUMCORES=$( expr 16 '*' "$NUMWORKERS")
 
 #Submtting a Spark Text Mining query (total words) to the Spark Master ($hostmaster), using all the cores available in the Spark Cluster ($NUMCORES)
 cd $HOME/defoe
-$SPARK_HOME/bin/spark-submit --master spark://$hostmaster:7077 --executor-memory 60g --py-files defoe.zip defoe/run_query.py data.txt papers defoe.papers.queries.total_words -n $NUMCORES > query_job_example.xt
+$SPARK_HOME/bin/spark-submit --master spark://$hostmaster:7077 --executor-memory 60g --py-files defoe.zip defoe/run_query.py data.txt papers defoe.papers.queries.total_words -n $NUMCORES > query_job.xt
